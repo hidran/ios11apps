3e72b27bd06e8bb7c6fa61f192e48ce693cdb84a
Hidran Arias <hidran@gmail.com>
2017-08-13 22:35:34 +0200

    add pokemon api

---
+++ tableviews/pokemon/table-view-controller/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PokemonDetailViewController.swift
+//  table-view-controller
+//
+//  Created by Hidran Arias on 07/08/17.
+//  Copyright © 2017 Hidran Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var pokemonDetail:String?
+    
+    @IBOutlet weak var pokImg: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pokemonName.text = pokemonDetail
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didClickBack(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+       // performSegue(withIdentifier: "backToPokemonList", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
