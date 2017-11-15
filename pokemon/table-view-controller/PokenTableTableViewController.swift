d1774796da67b0c60b41661498006f79afa1bf09
Hidran Arias <hidran@gmail.com>
2017-11-15 22:27:10 +0100

    add sorted pokemon

---
+++ pokemon/table-view-controller/PokenTableTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  PokenTableTableViewController.swift
+//  table-view-controller
+//
+//  Created by Hidran Arias on 07/08/17.
+//  Copyright © 2017 Hidran Arias. All rights reserved.
+//
+
+import UIKit
+
+class PokenTableTableViewController: UITableViewController {
+    var pokemons:[PokemonList] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       Pokemon.getPokemons(self)
+    }
+      
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return pokemons.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let pok = pokemons[indexPath.row]
+        cell.textLabel?.text = pok.name
+        cell.imageView?.image = pok.getImage()
+                
+        
+        return cell
+    }
+    
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      let pok = pokemons[indexPath.row]
+        performSegue(withIdentifier: "pokDetail", sender:pok )
+    
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "pokDetail" {
+            //PokemonDetailViewController
+            let vc = segue.destination as! PokemonDetailViewController
+            let poke = sender as! PokemonList
+            vc.pokemonDetail = poke
+        }
+    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
